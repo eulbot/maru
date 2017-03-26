992b120ce1c46b27abfdd95639669f96ea6daa37
Ulrich Fischer <careforthegoodones@gmail.com>
2017-03-26 20:11:19 +0200

    Add project files.

---
+++ Client/server.ts
@@ -0,0 +1,10 @@
+﻿import * as e from 'express'
+
+let server = e();
+const port = process.env.port || 1337;
+
+server.use('/', e.static(__dirname.concat('/Views')));
+
+server.listen(1337, () => {
+    console.log('listenting on', port);
+});
